b384b3b9162b93fbcd6b426aa473853a9870df15
JacobMJones <mrjacobtaiwa@gmail.com>
2018-05-30 15:27:58 -0400

    refactored gameManager object

--- public/scripts/mental.js
+++ public/scripts/mental.js
@@ -1,24 +1,3 @@
-/*
-1. Establish amount of players
-2. Establish win condition
-3. Create gameManager object
-
-
-
-
-making decks of various sizes is trivial, implement later
-let cardsPerDeck;
-function makeDeck() {
-    for (var i = 0; i < cardsPerDeck; i++) {
-
-    }
-}
-
-
-//server waits for each play
-*/
-
-//let amoutOfPlayers = 3;
 let winCondition;
 let pointsForVictory; //points can be either total points or total rounds, same variable
 
@@ -27,34 +6,42 @@ let gameManager = {};
 function setUpGame() {
     const deck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
     let amountOfPlayers = 3;
-    addGameConditionsToGameManager(deck);
+    //THESE WILL BE IN MONGO
+    addGameConditionsToGameManager(deck, amountOfPlayers, 'points');
     addPlayersToGameManager(amountOfPlayers, deck);
-    console.log(gameManager);
+
+    // console.log(gameManager);
 }
 
-function addGameConditionsToGameManager(deck) {
+function addGameConditionsToGameManager(deck, amountOfPlayers, win_condition) {
     //id will be set by database
     gameManager.game_id = 1;
     gameManager.targetDeck = deck;
     gameManager.game_start = new Date;
     //win condition can be points or matches
-    gameManager.win_condition = 'points';
+    gameManager.win_condition = win_condition;
     gameManager.total_rounds = deck.length;
 
+
+
 }
+//we need a connection from userDatabase for the in game name?
 
+
+//user object nested with info
 function addPlayersToGameManager(amountOfPlayers, deck) {
+
+    gameManager.players = {};
+
+    //the player object should have a relation to the players user/login name
     for (var i = 0; i < amountOfPlayers; i++) {
-
-        let player_id = `player${i}_id`;
-        let player_score = `player${i}_score`;
-        let player_has_played = `player${i}_has_played`;
-        let player_cards = `player${i}_cards`;
-
-        gameManager[player_id] = `p${i}_id`;
-        gameManager[player_score] = 0;
-        gameManager[player_has_played] = false;
-        gameManager[player_cards] = deck;
+        let player = `player${i}`;
+        gameManager.players[player] = {
+            player_score: 0,
+            player_has_played: false,
+            current_card_played: 0,
+            player_cards: deck,
+        }
     }
 }
 
@@ -79,7 +66,7 @@ function selectCardFromDeck(deck) {
     let indexForChoosingCard = Math.floor(Math.random() * cardsRemaining + 1);
     let cardToReturn = deck[indexForChoosingCard];
 
-    //remove from deck 
+    //remove from deck (will be removed from object on database)
     let index = deck.indexOf(cardToReturn);
     if (index > -1) {
         deck.splice(index, 1);
@@ -89,5 +76,18 @@ function selectCardFromDeck(deck) {
     console.log('target deck', gameManager.targetDeck);
 
 }
+
+function calculateWinnerOfTurns() {
+    console.log('in calculate winners');
+
+    let highestCard = 0;
+    for (var card in gameManager.players) {
+        console.log("in for in loop");
+    }
+}
 setUpGame();
 startGame();
+calculateWinnerOfTurns();
+
+//Match is the entire game
+//Round is the comprised of 13 turns
